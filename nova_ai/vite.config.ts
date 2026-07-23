3a864b8a109c6388a77331fcafabaec53811bc29
StellArc24 <stellararcgames@gmail.com>
2026-07-23 14:56:45 +0530

    Update vite.config.ts

--- nova_ai/vite.config.ts
+++ nova_ai/vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/NOVA_AI/',
+  base: '/nova_ai/',
   plugins: [
     vue(),
     tailwindcss(),
