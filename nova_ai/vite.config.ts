b63195041a8fe32dc9b6af113221cda03441cb09
StellArc24 <stellararcgames@gmail.com>
2026-07-28 00:28:52 +0530

    Update vite.config.ts

--- nova_ai/vite.config.ts
+++ nova_ai/vite.config.ts
@@ -1,19 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/nova_ai/',
+
+  server: { 
+    host: true, 
+    // Allow any tunnel host domain (Serveo, Ngrok, VS Code, etc.)
+    allowedHosts: true, 
+  },
+
   plugins: [
     vue(),
     tailwindcss(),
     vueJsx(),
-    vueDevTools(),
+    vueDevTools(), 
   ],
   resolve: {
     alias: {
